6b05bb1d07d30f6fdc4877d90556720a5eb687bd
root <jerry_hu123@163.com>
2018-07-07 10:07:49 +0800

    0707

--- images.sh
+++ images.sh
@@ -1,45 +1,43 @@
 #!/bin/bash
+
+#该脚本是将kubernetes相关镜像上传到aliyun
+
+#查看gcr.io镜像
+#https://console.cloud.google.com/kubernetes/images/list?location=GLOBAL&project=google-containers
+#通过下面的网址查看依赖镜像的版本号：
+#https://kubernetes.io/docs/admin/kubeadm/
 set -o errexit
 set -o nounset
 set -o pipefail
 
-KUBE_VERSION=v1.7.2
+KUBE_VERSION=v1.9.1
+HYPERKUBE_VERSION=v1.9.1_coreos.0
 KUBE_PAUSE_VERSION=3.0
-ETCD_VERSION=3.0.17
-DNS_VERSION=1.14.4
-
-yum remove docker docker-common docker-selinux docker-engine
-yum install -y yum-utils device-mapper-persistent-data lvm2
-yum-config-manager --add-repo https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
-yum makecache fast
-yum install -y docker-ce
-
-systemctl enable docker
-systemctl start docker
-
-tee -a /etc/sysctl.conf<<EOF
-net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
-EOF
-sysctl -p
+ETCD_VERSION=3.1.10
+DNS_VERSION=1.14.7
 
 GCR_URL=gcr.io/google_containers
 ALIYUN_URL=registry.cn-hangzhou.aliyuncs.com/szss_k8s
 
-images=(kube-proxy-amd64:${KUBE_VERSION}
+images=(
+kube-proxy-amd64:${KUBE_VERSION}
 kube-scheduler-amd64:${KUBE_VERSION}
 kube-controller-manager-amd64:${KUBE_VERSION}
 kube-apiserver-amd64:${KUBE_VERSION}
-pause-amd64:${KUBE_PAUSE_VERSION}
+#pause-amd64:${KUBE_PAUSE_VERSION}
 etcd-amd64:${ETCD_VERSION}
 k8s-dns-sidecar-amd64:${DNS_VERSION}
 k8s-dns-kube-dns-amd64:${DNS_VERSION}
-k8s-dns-dnsmasq-nanny-amd64:${DNS_VERSION})
+k8s-dns-dnsmasq-nanny-amd64:${DNS_VERSION}
+)
 
 
 for imageName in ${images[@]} ; do
-  docker pull $ALIYUN_URL/$imageName
-  docker tag $ALIYUN_URL/$imageName $GCR_URL/$imageName
-#  docker push $ALIYUN_URL/$imageName
-  docker rmi $ALIYUN_URL/$imageName
+  docker pull $GCR_URL/$imageName
+  docker tag $GCR_URL/$imageName $ALIYUN_URL/$imageName
 done
+
+#for imageName in ${images[@]} ; do
+#  docker push $ALIYUN_URL/$imageName
+#  docker rmi $ALIYUN_URL/$imageName
+#done
